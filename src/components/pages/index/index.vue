6c00b5a8c3b62e713d61548b5f9e23a953f47aef
anna <czinkele1314@gmail.com>
2023-11-21 15:25:18 +0800

    详情跳转页123

--- src/components/pages/index/index.vue
+++ src/components/pages/index/index.vue
@@ -3,23 +3,21 @@
         <ul>
             <li><router-link to="/">首页</router-link></li>
             <li><router-link to="news">新闻页面</router-link></li>
-            <li><span @click="goPage('/goods')">产品页面</span></li>
+            <li><span @click="goPage('/goods/item')">产品页面</span></li>
             <li><span @click="goPage('/login')">会员登录</span></li>
         </ul>
     </div>
 </template>
 
 <script>
-
-
     export default{
-    name: "index",
-    methods:{
-        goPage(url){
+        name: "index",
+        methods:{
+            goPage(url){
             this.$router.push(url)
+            }
         }
     }
-}
 
 </script>
 
--- src/pages/goods/index.vue
+++ src/pages/goods/index.vue
@@ -2,9 +2,9 @@
     <div>
         <button type="button" @click="$router.go(-1)">返回</button>
         <ul>
-            <li @click="goods('/goods/itme')">商品</li>
-            <li @click="goods('/goods/content')">详情</li>
-            <li @click="goods('/goods/review')">评价</li>
+            <li @click="goPage('/goods/item')">商品</li>
+            <li @click="goPage('/goods/content')">详情</li>
+            <li @click="goPage('/goods/review')">评价</li>
         </ul>
         <div>
            <router-view></router-view>
